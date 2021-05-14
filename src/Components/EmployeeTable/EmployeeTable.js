e187f61f48ec2de51dacc585c9d9c8fd911db574
Michael Knowlton <mknowlton89@gmail.com>
2021-05-14 19:56:59 -0400

    Updated the sort functionality and got it working. I think there is still some tweaking to do to make it a bit more performant. But it works for now.

--- src/Components/EmployeeTable/EmployeeTable.js
+++ src/Components/EmployeeTable/EmployeeTable.js
@@ -3,28 +3,30 @@ import './styles.css'
 
 export function EmployeeTable({ employees, setEmployeeList, originalEmployeeList }) {
 
-    // console.log(props);
     function handleSortFunction(event) {
         if (event.target.name === "email") {
-            let sortedList = employees.sort((a, b) => a.email.localeCompare(b.email));
+            let sortedList = [...employees].sort((a, b) => a.email.localeCompare(b.email));
+            setEmployeeList(sortedList);
+        } else if (event.target.name === "name") {
+            let sortedList = [...employees].sort((a, b) => a.name.first.localeCompare(b.name.first));
             setEmployeeList(sortedList);
         }
     };
 
-    console.log(originalEmployeeList);
+    console.log(employees);
 
     return (
         <div className="employeeTableContainer">
             <table className="table">
-                <tr>
-                    <th>Photo</th>
-                    <th>Name</th>
-                    <th>Email <button name="email" onClick={handleSortFunction}>^</button></th>
-                    <th>DOB</th>
+                <tr id="header">
+                    <th>Headshot</th>
+                    <th>Name<button className="sortBtn" name="name" onClick={handleSortFunction}>^</button></th>
+                    <th>Email <button className="sortBtn" name="email" onClick={handleSortFunction}>^</button></th>
+                    <th>Phone Number</th>
                 </tr>
                 {employees.length > 0 ? employees.map((employeeData, index) => (
                     <tr key={index}>
-                        <td><img src={employeeData.picture.thumbnail} /></td>
+                        <td><img src={employeeData.picture.thumbnail} alt={"Headshot of " + employeeData.name.first + " " + employeeData.name.last} /></td>
                         <td>{employeeData.name.first} {employeeData.name.last}</td>
                         <td>{employeeData.email}</td>
                         <td>{employeeData.phone}</td>
