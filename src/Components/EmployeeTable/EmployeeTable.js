aa33a0fc566112cf8fae6821ed070ca1d529fcde
Michael Knowlton <mknowlton89@gmail.com>
2021-05-13 20:40:42 -0400

    Built a hard-coded table within the EmployeeTable.js component to get an idea of what I need to do.

--- src/Components/EmployeeTable/EmployeeTable.js
+++ src/Components/EmployeeTable/EmployeeTable.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './styles.css'
 
-export function EmployeeTable() {
+export function EmployeeTable(props) {
     return(
-        <h1>Employee Table</h1>
+        <div className="employeeTableContainer">
+            <table className="table">
+                <tr>
+                    <th>Name</th>
+                    <th>Position</th>
+                    <th>Department</th>
+                    <th>Phone Number</th>
+                </tr>
+                <tr>
+                    <td>Michael</td>
+                    <td>Jr Software Engineer</td>
+                    <td>Engineering</td>
+                    <td>770-843-4662</td>
+                </tr>
+                <tr>
+                    <td>Heather</td>
+                    <td>Sr Software Engineer</td>
+                    <td>Special Projects</td>
+                    <td>770-843-4663</td>
+                </tr>
+                <tr>
+                    <td>Cameron</td>
+                    <td>BehBeh</td>
+                    <td>N/A</td>
+                    <td>770-843-4664</td>
+                </tr>
+            </table>
+        </div>
     )
 };
